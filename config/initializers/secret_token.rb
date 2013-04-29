8cf1eb48e98f5fd2da997f4e221c48fc374bd362
Preetam Joshi <preetamjoshi93@gmail.com>
2013-04-29 00:53:05 -0700

    test app ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRails3213::Application.config.secret_token = 'ac91e7b9b6bd42413301e2acc6a8f2aab84fac82d754e17401d0c19a21aadd9a24409cba6216a2d76c45f473129f984d86341555ee4c484f63c366f271e1155e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyRails3213::Application.config.session_store :cookie_store, key: '_myRails3213_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyRails3213::Application.config.session_store :active_record_store
